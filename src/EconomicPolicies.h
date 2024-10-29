809b8cdb29a873cfe9efa8d92ff28e6365181524
Nevan Sami Rahman <133259091+rsnevan@users.noreply.github.com>
2024-10-29 23:06:13 +0200

    Adding Doxygen comments.

--- src/EconomicPolicies.h
+++ src/EconomicPolicies.h
@@ -1,25 +1,45 @@
-#include <exception>
-using namespace std;
+/**
+ * @file EconomicPolicies.h
+ * @brief Economic policy implementation class
+ * @author Design Wits
+ * @date 2024-04-29
+ */
 
 #ifndef ECONOMICPOLICIES_H
 #define ECONOMICPOLICIES_H
 
-// #include "PublicServicesPolicies.h"
-// #include "Citizen.h"
 #include "Policies.h"
+#include "PublicServicesPolicies.h"
+#include "Citizen.h"
 
 class PublicServicesPolicies;
 class Citizen;
-// class Policies;
-class EconomicPolicies;
 
-class EconomicPolicies: public Policies
-{
-	public: 
-		explicit EconomicPolicies(Government* govt); // Command Pattern for policies
-		PublicServicesPolicies* psPolicies;
-	    Citizen* citizen;
-	    void executePolicy() override;
+/**
+ * @class EconomicPolicies
+ * @brief Concrete implementation of economic policies
+ * 
+ * This class handles the execution of economic policies affecting
+ * citizens and public services. It acts as a ConcreteCommand in
+ * the Command pattern hierarchy.
+ */
+class EconomicPolicies : public Policies {
+public:
+    /**
+     * @brief Construct a new Economic Policies object
+     * @param govt Pointer to the government executing the policies
+     */
+    explicit EconomicPolicies(Government* govt);
+
+    PublicServicesPolicies* psPolicies;  ///< Related public services policies
+    Citizen* citizen;                    ///< Affected citizen
+
+    /**
+     * @brief Implementation of policy execution for economic policies
+     * 
+     * Defines the specific behavior for executing economic policies.
+     */
+    void executePolicy() override;
 };
 
-#endif
+#endif // ECONOMICPOLICIES_H
