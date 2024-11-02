db9c093f2b1d2589fd3f4ca2b9f0c9ebc60ca463
un4thi <u22553402@tuks.co.za>
2024-11-02 11:08:54 +0200

    Update LandmarkBuilding.h

--- src/LandmarkBuilding.h
+++ src/LandmarkBuilding.h
@@ -1,36 +1,49 @@
-#include <exception>
+#include "Buildings.h"
+#include <string>
 using namespace std;
 
-#ifndef __LandmarkBuilding_h__
-#define __LandmarkBuilding_h__
+#ifndef LANDMARKBUILDING_H
+#define LANDMARKBUILDING_H
 
-#include "State.h"
-#include "Buildings.h"
-
+class Citizen;
+class ResourceMediator;
 class State;
-// class Buildings;
-class LandmarkBuilding;
 
-class LandmarkBuilding: public Buildings
-{
-	private: State _state;
-	private: int _powerReq;
-	private: int _waterReq;
-	private: double _maintenanceCost;
-	private: int _sewageCost;
-	private: int _waste;
+class LandmarkBuilding : Buildings {
 
-	public: void requestState();
+protected:
+    // Basic building details
+    string buildingName;
+    string buildingType;
+    Citizen* listOfCitizens;
 
-	public: double getMaintenanceCost();
+    // Resource and cost requirements
+    double maintenanceCost;
+    int powerReq;
+    int waterReq;
+    int sewageCost;
 
-	public: int getPowerReq();
+    // Mediator and state objects
+    ResourceMediator* resources;
+    State* state;
 
-	public: int getSewageCost();
+public:
+    // Pure virtual functions for building operations
+    virtual bool cleanSewage() = 0;
+    virtual bool cleanWaste() = 0;
+    virtual string getBuildingType() = 0;
+    virtual int getJobsCreated() = 0;
+    virtual double getMaintenanceCost() = 0;
+    virtual string getName() = 0;
+    virtual int getPowerReq() = 0;
+    virtual int getSewageCost() = 0;
+    virtual int getWaterCost() = 0;
+    virtual void operation() = 0;
+    virtual bool payMaintenance() = 0;
+    virtual bool receivePower() = 0;
+    virtual bool receiveWater() = 0;
+    virtual void requestState() = 0;
 
-	public: int getWaterCost();
-
-	public: int getWasteCost();
 };
 
 #endif
