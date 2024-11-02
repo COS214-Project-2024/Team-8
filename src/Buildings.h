88486711ab3ec7918e04a5c1bca2a6883280ee8b
un4thi <u22553402@tuks.co.za>
2024-11-02 11:01:17 +0200

    Update Buildings.h

--- src/Buildings.h
+++ src/Buildings.h
@@ -1,44 +1,48 @@
-#include <exception>
+#include <string>
 using namespace std;
-
-#ifndef __Buildings_h__
-#define __Buildings_h__
-
-#include "State.h"
-// #include "Government.h"
-// #include "ConstructionCompany.h"
-// #include "Resources.h"
+#ifndef BUILDINGS_H
+#define BUILDINGS_H
 
 class State;
-class Government;
-class ConstructionCompany;
-class Resources;
-class Buildings;
+class ResourceMediator;
+class Citizen;
 
-__abstract class Buildings
-{
-	private: State _state;
-	private: int _powerReq;
-	private: int _waterReq;
-	private: double _maintenanceCost;
-	private: int _sewageCost;
-	private: int _waste;
-	public: Government* _unnamed_Government_;
-	public: ConstructionCompany* _unnamed_ConstructionCompany_;
-	public: State* _buidling;
-	public: Resources* _unnamed_Resources_;
+class Buildings {
+public:
+    State* state;
 
-	public: virtual void requestState() = 0;
+protected:
+    ResourceMediator* resources;
+    int powerReq;
+    int waterReq;
+    int sewageCost;
+    double maintenanceCost;
+    string buildingName;
+    string buildingType;
+    Citizen* listOfCitizens;
 
-	public: virtual double getMaintenanceCost() = 0;
+public:
+    // Pure virtual functions to be implemented by derived classes
+    virtual void requestState() = 0;
+    virtual double getMaintenanceCost() = 0;
+    virtual int getPowerReq() = 0;
+    virtual int getSewageCost() = 0;
+    virtual int getWaterCost() = 0;
+    virtual string getName() = 0;
+    virtual string getBuildingType() = 0;
+    virtual int getJobsCreated() = 0;
 
-	public: virtual int getPowerReq() = 0;
+    // Resource management functions
+    virtual bool receivePower() = 0;
+    virtual bool receiveWater() = 0;
+    virtual bool payMaintenance() = 0;
+    virtual bool cleanWaste() = 0;
+    virtual bool cleanSewage() = 0;
 
-	public: virtual int getSewageCost() = 0;
+    // General operation function
+    virtual void operation() = 0;
 
-	public: virtual int getWaterCost() = 0;
-
-	public: virtual void getWasteCost() = 0;
 };
 
+#endif
 #endif
