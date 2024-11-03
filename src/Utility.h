9871ef6d47596b9520f26715668d0f8437712f62
UML23 <u23629810@tuks.co.za>
2024-11-03 11:26:43 +0200

    Merge utility.h

--- src/Utility.h
+++ src/Utility.h
@@ -2,7 +2,6 @@
 #define UTILITY_H
 
 #include <iostream>
-<<<<<<< HEAD
 #include <thread>
 #include <chrono>
 #include <vector>
@@ -50,52 +49,6 @@ public:
 	virtual void switchFuel();
 
 	virtual void setMaxGallons(float max);
-=======
-#include "UtilityManager.h"
-class EnergySource;
-class Utility {
-protected:
-	std::string status;
-	//Buildings* buildinglist;
-	UtilityManager* manager;
-
-public:
-	virtual std::string getStatus();
-
-	virtual void setFuel(EnergySource* fuel);
-
-	virtual void executeOperation() = 0;
-
-	virtual void setmaxWaste(int max);
-
-	virtual void repairUtility() = 0;
-
-	virtual std::string getEnergyType();
-
-	virtual void setProcessCapacity(int pro);
-
-	virtual int getCurrentSupply();
-
-	virtual int getCurrentLoad();
-
-	virtual void undoChange() = 0;
-
-	virtual void setMaxWatts(float max);
-
-	virtual int getCurrentOutput();
-
-	virtual void setMaxGallons(float max);
-
-	virtual void pauseOperation() = 0;
-
-	virtual void addCommand(Command* com) = 0;
-
-	virtual float getMaxWatts();
-
-	virtual std::string getUtilityType() = 0;
-
-	virtual ~Utility();
->>>>>>> integration
 };
 
 #endif
