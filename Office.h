a4add762a99d09682bf24f5b3725bd3a37b4e65a
WizardOfGold <168462349+WizardOfGold@users.noreply.github.com>
2024-11-03 19:21:40 +0200

    Update Office.h

--- Office.h
+++ Office.h
@@ -191,7 +191,7 @@ public:
      * @brief gets the current income of a commercial building
      * @return the income as an integer
      */
-    virtual void SetBalance(double b);
+    virtual void setBalance(double b);
 };
 
 #endif
