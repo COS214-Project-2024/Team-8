48b4a54cde6a46a7103c7e3caecc89e9ca6e3fe6
un4thi <u22553402@tuks.co.za>
2024-11-03 20:13:47 +0200

    Update Park.cpp

--- Park.cpp
+++ Park.cpp
@@ -21,7 +21,7 @@ Park::Park(string bName,int pReq, int wReq, double mCost, int sCost, int waste)
 
 	this->state =  new UnderConstruction(); //when we instantiate a building it will be under construction
 	this->state->handle(); //handle the state;
-	this->setState(); //change the state to operation if the resources are available
+	this->state->changeState();//in case it needs to change
 
 	this->listOfCitizens = new Citizen("NewCitizen",100,21,5.0);
 	this->listOfCitizens->adjustCitizenSatisfaction(5.0);
