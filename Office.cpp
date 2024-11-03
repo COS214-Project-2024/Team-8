0ec9aa83570331d2d8b8b5fa28b03217f7409b11
un4thi <u22553402@tuks.co.za>
2024-11-03 15:48:50 +0200

    Update Office.cpp - remove useless line
    removing the line that instantiated a new resource mediator because we now have a setResources function.

--- Office.cpp
+++ Office.cpp
@@ -26,7 +26,6 @@ Office::Office(string bName,int pReq, int wReq, double mCost, int sCost, int was
 	this->state->handle(); //handle the state;
 	this->setState(); //change the state to operation if the resources are available
 
-	this->resources = new ResourceMediator();
 	this->listOfCitizens = new Citizen("NewCitizen",100,21,5.0);
 	this->listOfCitizens->adjustCitizenSatisfaction(5.0);
 }
