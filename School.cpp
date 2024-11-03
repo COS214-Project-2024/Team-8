81ef262b323bda40cc6cacfa5a7d2a57253d0b0c
un4thi <u22553402@tuks.co.za>
2024-11-03 20:14:55 +0200

    Update School.cpp

--- School.cpp
+++ School.cpp
@@ -22,7 +22,7 @@ School::School(string bName,int pReq, int wReq, double mCost, int sCost, int was
 
 	this->state =  new UnderConstruction(); //when we instantiate a building it will be under construction
 	this->state->handle(); //handle the state;
-	this->setState(); //change the state to operation if the resources are available
+	this->state->changeState();//in case it needs to change
 
 	this->listOfCitizens = new Citizen();
 }
