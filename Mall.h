ec63269e0cd745d71d04bdea0a2765093b07f10e
un4thi <u22553402@tuks.co.za>
2024-11-03 18:51:18 +0200

    Update Mall.h

--- Mall.h
+++ Mall.h
@@ -161,6 +161,24 @@ public:
      * @return The payment amount as an integer.
      */
     int payCitizen();
+
+ /**
+     * @brief gets the current income of a commercial building
+     * @return the income as an integer
+     */
+    virtual int getCommercialIncome();
+
+    /**
+     * @brief gets the current income of a commercial building
+     * @return the income as an integer
+     */
+    virtual double getBalance();
+
+    /**
+     * @brief gets the current income of a commercial building
+     * @return the income as an integer
+     */
+    virtual void SetBalance(double b);
 };
 
 #endif
