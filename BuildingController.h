139bf88740c4139ff37864ef5d8ab0246c58081b
WizardOfGold <168462349+WizardOfGold@users.noreply.github.com>
2024-11-04 14:50:58 +0200

    Add files via upload
    Added some Doxygen to these files

--- BuildingController.h
+++ BuildingController.h
@@ -11,28 +11,92 @@
 class BuildingController
 {
 private:
+    /**
+     * @class ListOfBuildings
+     * @brief Lists of Buildings
+     * Class uses a list of Buildings to loop through each Building to acquire data
+     */
+    Buildings **ListOfBuildings;
+    /**
+     * @class amountOfBuildings
+     * @brief Amount of Buildings in the list
+     * Variable to hold the amount of Buildings To loop through the list
+     */
+    int amountOfBuildings;
 
-    Buildings ** ListOfBuildings ; 
-    int amountOfBuildings ; 
 public:
     BuildingController(/* args */);
     ~BuildingController();
+    /**
+     * @class addBuilding
+     * @brief Adds a Building to the list
+     * Adds the Building to the list to be used by the Interface
+     */
+    void addBuilding(Buildings *add);
+    /**
+     * @class removeBuilding
+     * @brief Removes A Building From the List
+     * Removes the Building from the list.
+     */
+    void removeBuilding(Buildings *rem);
+    /**
+     * @class getAmountOfBuildings
+     * @brief Returns the amount of Buildings
+     * Returns the amount of Buildings in the list
+     * The Player could see the amount of Buildings in the city for clarity
+     */
+    int getAmountOfBuildings();
 
-    
-    void addBuilding(Buildings * add) ;
-    void removeBuilding(Buildings * rem) ;
-    int getAmountOfBuildings() ; 
-
-    int getTotalPowerReq() ; 
-    int getTotalSewageReq() ; 
-    int getTotalWasteReq() ; 
-    int getTotalWaterReq() ; 
-    int getMaintenanceCost() ; 
-
-    Citizen** getListOfCitizens() ; 
-    int getCommercialIncome() ; 
-
-
+    /**
+     * @class getTotalPowerReq
+     * @brief Returns the total amount of Power Requirment
+     * Returns the amount of Power Requirment in the City
+     * The Player could see the amount of Power that the city needs
+     *
+     */
+    int getTotalPowerReq();
+    /**
+     * @class getTotalSewageReq
+     * @brief Returns the total amount of Sewage Requirment
+     * Returns the amount of Sewage Requirment in the City
+     * The Player could see the amount of Sewage Managment that the city needs
+     *
+     */
+    int getTotalSewageReq();
+    /**
+     * @class getTotalWasteReq
+     * @brief Returns the total amount of Waste Managment Requirment
+     * Returns the amount of Waste Managment that the City can Handle .
+     * The Player could see the amount of Sewage Managment that the city needs
+     */
+    int getTotalWasteReq();
+    /**
+     * @class getTotalWaterReq
+     * @brief Returns the total amount of Water Requirment 
+     * Returns the amount of Water Requirment that the City needs .
+     * The Player could see the amount of Water that the city needs .
+     */
+    int getTotalWaterReq();
+    /**
+     * @class getMaintenanceCost
+     * @brief Returns the amount of Maintenance cost for the Buildings
+     * Returns the amount of Maintenance Funds that all the Buildings in the city needs to stay operational 
+     * The Player could see the Total Mainenance cost of the City
+     */
+    int getMaintenanceCost();
+    /**
+     * @class getListOfCitizens
+     * @brief Returns a List of Citizens in a Residential Building
+     * Loops through the list and returns a list of all the Citizens that is inside of homes .
+     */
+    Citizen **getListOfCitizens();
+    /**
+     * @class getCommercialIncome
+     * @brief Returns the amount of Commerce that the Commercial Buildings Generate
+     * Returns the amount of Income That the Commercial Buildings Generate 
+     * Is used in unisen with the Taxable Income to see the total amount of Income the City Generates
+     */
+    int getCommercialIncome();
 };
 
 #endif
--- CitizenController.h
+++ CitizenController.h
@@ -7,31 +7,102 @@ class CitizenController
 {
 private:
     /* data */
-    Citizen ** ListOfCitizens ; 
-    int amountOfCitizens ; 
-    int amountEmployedCitizens ;
-    int amountPropertyOwners ; 
-
-    int AvgSatisfaction ; 
-
-    float TaxableIncome ; 
+    /**
+     * @class CitizenController
+     * @brief Lists of Citizens
+     * Class uses a list of Citizens to loop through each Citizen to acquire data
+     */
+    Citizen **ListOfCitizens;
+    /**
+     * @class amountOfCitizens
+     * @brief Amount of Citizens in the list
+     * Variable to hold the amount of Citizens To loop through the list
+     */
+    int amountOfCitizens;
+    /**
+     * @class amountEmployedCitizens
+     * @brief Amount of Employed Citizens
+     * Shows the number of employed citizens in the city
+     */
+    int amountEmployedCitizens;
+    /**
+     * @class amountPropertyOwners
+     * @brief Amount of Property Owners Citizens
+     * Shows the number of Property Owners in the city
+     */
+    int amountPropertyOwners;
+    /**
+     * @class AvgSatisfaction
+     * @brief Average Satisfaction of the Citizens
+     * Holds the average satisfaction of the citizens
+     * Thus the player can see the happinness of the city
+     */
+    int AvgSatisfaction;
+    /**
+     * @class TaxableIncome
+     * @brief Taxable Income of the Citizens
+     * The Amount of Taxes that the Citizens pay
+     * This variable is used with the Income from Commercial Buildings
+     */
+    float TaxableIncome;
 
 public:
     CitizenController();
     ~CitizenController();
+    /**
+     * @class addCitizen
+     * @brief Adds a Citizen too a list
+     * Adds the Citizen to the list to be used by the Interface
+     */
+    void addCitizen(Citizen *add);
+    /**
+     * @class removeCitizen
+     * @brief Removes A Citizen From the List
+     * Removes the Citizen from the list.
+     * This could be due to either the Citizen leaves the city or the Citizen passed away
+     */
+    void removeCitizen(Citizen *rem);
 
-    void addCitizen(Citizen * add) ;
-    void removeCitizen(Citizen * rem) ;
-
-    int getAmountOfCitizens() ; 
-    int getEmployedCitizensAmount() ; 
-    int getamountPropertyOwnersAmount() ; 
-
-    double getAvgSatisfaction() ;
-    float GetTaxableIncome() ;
-
-    Citizen** getListOfCitizens() ;   
-
+    /**
+     * @class getAmountOfCitizens
+     * @brief Returns the size of the List
+     * Returns the size of The citizen list in order for the player to see the
+     * maximum population of the city
+     */
+    int getAmountOfCitizens();
+    /**
+     * @class getEmployedCitizensAmount
+     * @brief Returns the amount of the Employed Citizens
+     * Returns the size of The citizen that holds employment
+     */
+    int getEmployedCitizensAmount();
+        /**
+     * @class getamountPropertyOwnersAmount
+     * @brief Returns the amount of the Property Owners
+     * Returns the size of The citizen that holds Property
+     */
+    int getamountPropertyOwnersAmount();
+    /**
+     * @class getAvgSatisfaction
+     * @brief Returns the average satisfaction of the city
+     * Loops through the list and adds the amount of Satisfaction , afterwards divides by the population of the city 
+     * Returns the new amount back to the City for the Player to see
+     */
+    double getAvgSatisfaction();
+    /**
+     * @class GetTaxableIncome
+     * @brief Returns the total Taxable Income of the city
+     * Loops through the list and adds the amount of Taxable Income from the Citizens
+     * This value is added with Commercial Income to see the Income of the Funds .
+     */
+    float GetTaxableIncome();
+    /**
+     * @class getListOfCitizens
+     * @brief Returns the list Of Citizens
+     * Function returns the list of Citizens .
+     * This is here if any functions above the City wants to be able to use the Citizens
+     */
+    Citizen **getListOfCitizens();
 };
 
 #endif
