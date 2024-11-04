bee2c237354777278cc03e4ceefa0b33048a9b4d
Palesa <thabanepalesa016@gmail.com>
2024-11-04 22:58:53 +0200

    Updated TaxationSystemDocTest.cpp

--- src/tests/TaxationSystemDocTest.cpp
+++ src/tests/TaxationSystemDocTest.cpp
@@ -1,198 +1,223 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
-#include <iostream>
-#include <vector>
-#include <memory>
-
-// Include all necessary headers
-#include "ResidentialTaxationSystem.h"
-#include "CommercialTaxationSystem.h"
+#include "doctest.h"
 #include "BudgetAllocationSystem.h"
+#include "CommercialTaxationSystem.h"
+#include "ResidentialTaxationSystem.h"
 #include "FinanceDepartment.h"
 #include "Government.h"
+#include "Shop.h"
+#include "Mall.h"
+#include "Office.h"
 #include "Citizen.h"
-#include "CommercialBuilding.h"
 
-// ResidentialTaxationSystem Tests
-TEST_CASE("ResidentialTaxationSystem") {
-    ResidentialTaxationSystem taxSystem;
+TEST_CASE("Testing BudgetAllocationSystem") {
+    BudgetAllocationSystem* budgetSystem = new BudgetAllocationSystem();
+    double availableFunds = 1000.0;
 
-    SUBCASE("Income Tax Collection") {
-        // Redirect cout to capture output
-        std::streambuf* old_cout = std::cout.rdbuf();
-        std::stringstream ss;
-        std::cout.rdbuf(ss.rdbuf());
-
-        double taxRate = 0.2;  // 20% tax rate
-        double income = 50000.0;
-        double incomeTax = taxSystem.collectIncomeTax(taxRate, income);
-
-        // Restore cout
-        std::cout.rdbuf(old_cout);
-
-        CHECK_EQ(incomeTax, doctest::Approx(10000.0));
-        CHECK(ss.str().find("Income Tax Collected: R 10000") != std::string::npos);
+    SUBCASE("Medical Center Fund Allocation") {
+        double medicalFunds = budgetSystem->allocateMedicalCenterFunds(availableFunds);
+        CHECK(medicalFunds > 0);
+        CHECK(medicalFunds <= availableFunds);
     }
 
-    SUBCASE("Property Tax Collection") {
-        // Redirect cout to capture output
-        std::streambuf* old_cout = std::cout.rdbuf();
-        std::stringstream ss;
-        std::cout.rdbuf(ss.rdbuf());
-
-        double taxRate = 0.1;  // 10% tax rate
-        double income = 50000.0;
-        double propertyTax = taxSystem.collectPropertyTax(taxRate, income);
-
-        // Restore cout
-        std::cout.rdbuf(old_cout);
-
-        CHECK_EQ(propertyTax, doctest::Approx(5000.0));
-        CHECK(ss.str().find("Property Tax Collected: R 5000") != std::string::npos);
+    SUBCASE("School Fund Allocation") {
+        double schoolFunds = budgetSystem->allocateSchoolFunds(availableFunds);
+        CHECK(schoolFunds > 0);
+        CHECK(schoolFunds <= availableFunds);
     }
+
+    SUBCASE("Police Station Fund Allocation") {
+        double policeFunds = budgetSystem->allocatePoliceStationFunds(availableFunds);
+        CHECK(policeFunds > 0);
+        CHECK(policeFunds <= availableFunds);
+    }
+
+    delete budgetSystem;
 }
 
-// CommercialTaxationSystem Tests
-TEST_CASE("CommercialTaxationSystem") {
-    CommercialTaxationSystem taxSystem;
-
+TEST_CASE("Testing CommercialTaxationSystem") {
+    CommercialTaxationSystem* taxSystem = new CommercialTaxationSystem();
+    
     SUBCASE("Business Tax Collection") {
-        // Redirect cout to capture output
-        std::streambuf* old_cout = std::cout.rdbuf();
-        std::stringstream ss;
-        std::cout.rdbuf(ss.rdbuf());
-
-        double taxRate = 0.25;  // 25% tax rate
-        double businessProfit = 100000.0;
-        double businessTax = taxSystem.collectBusinessTax(taxRate, businessProfit);
-
-        // Restore cout
-        std::cout.rdbuf(old_cout);
-
-        CHECK_EQ(businessTax, doctest::Approx(25000.0));
-        CHECK(ss.str().find("Total Business Taxes Collected: R 25000") != std::string::npos);
+        double businessTaxRate = 0.15;
+        double businessProfit = 500000.0;
+        double businessTax = taxSystem->collectBusinessTax(businessTaxRate, businessProfit);
+        CHECK(businessTax == doctest::Approx(75000.0)); // 15% of 500000
     }
 
     SUBCASE("Sales Tax Collection") {
-        // Redirect cout to capture output
-        std::streambuf* old_cout = std::cout.rdbuf();
-        std::stringstream ss;
-        std::cout.rdbuf(ss.rdbuf());
-
-        double taxRate = 0.15;  // 15% tax rate
-        double businessProfit = 80000.0;
-        double salesTax = taxSystem.collectSalesTax(taxRate, businessProfit);
-
-        // Restore cout
-        std::cout.rdbuf(old_cout);
-
-        CHECK_EQ(salesTax, doctest::Approx(12000.0));
-        CHECK(ss.str().find("Total Sales Taxes Collected: R 12000") != std::string::npos);
+        double salesTaxRate = 0.10;
+        double salesAmount = 500000.0;
+        double salesTax = taxSystem->collectSalesTax(salesTaxRate, salesAmount);
+        CHECK(salesTax == doctest::Approx(50000.0)); // 10% of 500000
     }
+
+    delete taxSystem;
 }
 
-// BudgetAllocationSystem Tests
-TEST_CASE("BudgetAllocationSystem") {
-    // Note: Since BudgetAllocationSystem methods are not in a class, 
-    // we'll test the global functions directly
-
-    SUBCASE("Medical Center Funds Allocation") {
-        double availableFunds = 1000000.0;
-        double medicalCenterFunds = allocateMedicalCenterFunds(availableFunds);
-
-        CHECK_EQ(medicalCenterFunds, doctest::Approx(200000.0)); // 20% of available funds
+TEST_CASE("Testing ResidentialTaxationSystem") {
+    ResidentialTaxationSystem* taxSystem = new ResidentialTaxationSystem();
+    
+    SUBCASE("Income Tax Collection") {
+        double incomeTaxRate = 0.18;
+        double income = 350000.0;
+        double incomeTax = taxSystem->collectIncomeTax(incomeTaxRate, income);
+        CHECK(incomeTax == doctest::Approx(63000.0)); // 18% of 350000
     }
 
-    SUBCASE("School Funds Allocation") {
-        double availableFunds = 1000000.0;
-        double schoolFunds = allocateSchoolFunds(availableFunds);
-
-        CHECK_EQ(schoolFunds, doctest::Approx(100000.0)); // 10% of available funds
+    SUBCASE("Property Tax Collection") {
+        double propertyTaxRate = 0.05;
+        double propertyValue = 350000.0;
+        double propertyTax = taxSystem->collectPropertyTax(propertyTaxRate, propertyValue);
+        CHECK(propertyTax == doctest::Approx(17500.0)); // 5% of 350000
     }
 
-    // Add similar tests for other allocation functions
-    SUBCASE("Police Station Funds Allocation") {
-        double availableFunds = 1000000.0;
-        double policeStationFunds = allocatePoliceStationFunds(availableFunds);
-
-        CHECK_EQ(policeStationFunds, doctest::Approx(100000.0)); // 10% of available funds
-    }
+    delete taxSystem;
 }
 
-// FinanceDepartment Tests (Mock some dependencies)
-TEST_CASE("FinanceDepartment") {
-    // Mocking minimal dependencies
-    class MockCitizen : public CitizenInterface {
-    public:
-        std::string citizenType() override { return "Employed"; }
-        double getSalary() override { return 50000.0; }
-        double getBalance() override { return 100000.0; }
-        void setBalance(double bal) override { balance = bal; }
-    private:
-        double balance = 100000.0;
-    };
+TEST_CASE("Testing FinanceDepartment") {
+    FinanceDepartment* financeDept = new FinanceDepartment();
+    
+    SUBCASE("Setting Tax Rates") {
+        financeDept->setResidentialIncomeTaxRate(0.10);
+        financeDept->setResidentialPropertyTaxRate(0.012);
+        financeDept->setCommercialBusinessTaxRate(0.25);
+        financeDept->setCommercialSalesTaxRate(0.08);
+        
+        CHECK(financeDept->getResidentialIncomeTaxRate() == doctest::Approx(0.10));
+        CHECK(financeDept->getResidentialPropertyTaxRate() == doctest::Approx(0.012));
+        CHECK(financeDept->getCommercialBusinessTaxRate() == doctest::Approx(0.25));
+        CHECK(financeDept->getCommercialSalesTaxRate() == doctest::Approx(0.08));
+    }
 
-    class MockCommercialBuilding : public CommercialBuilding {
-    public:
-        double getProfit() override { return 200000.0; }
-        double getSales() override { return 500000.0; }
-        double getBalance() override { return 300000.0; }
-        void setBalance(double bal) override { balance = bal; }
-    private:
-        double balance = 300000.0;
-    };
+    /*SUBCASE("Adding Commercial Buildings") {
+        CommercialBuilding* shop = new Shop("TestShop", 100, 50, 2000.0, 100, 30, 1000.0f, 500.0f, 10);
+        CommercialBuilding* mall = new Mall("TestMall", 200, 100, 5000.0, 150, 50, 3000.0f, 1500.0f, 50);
+        
+        financeDept->addCommercialBuilding(shop);
+        financeDept->addCommercialBuilding(mall);
+        
+        CHECK(financeDept->getNumberOfCommercialBuildings() == 2);
+    }
 
-    SUBCASE("Tax Collection and Fund Allocation") {
-        std::vector<CitizenInterface*> residents = { new MockCitizen() };
-        std::vector<CommercialBuilding*> buildings = { new MockCommercialBuilding() };
+    SUBCASE("Adding Residents") {
+        for(int i = 0; i < 5; i++) {
+            CitizenInterface* citizen = new Citizen();
+            financeDept->addResidents(citizen);
+        }
+        
+        CHECK(financeDept->getNumberOfResidents() == 5);
+    }*/
 
-        FinanceDepartment financeDept(residents, buildings);
+    SUBCASE("Fund Allocation Requests") {
+        financeDept->setAvailableFunds(1000000.0);
+        
+        double publicServiceFunds = financeDept->delegateRequestForAllocationOfPublicServiceBuildingsFunds();
+        double landmarkFunds = financeDept->delegateRequestForAllocationOfLandmarkBuildingsFunds();
+        double transportFunds = financeDept->delegateRequestForAllocationOfTransportInfrastructureFunds();
+        double utilitiesFunds = financeDept->delegateRequestForAllocationOfUtilitiesFunds();
+        
+        CHECK(publicServiceFunds > 0);
+        CHECK(landmarkFunds > 0);
+        CHECK(transportFunds > 0);
+        CHECK(utilitiesFunds > 0);
+        
+        double totalAllocation = publicServiceFunds + landmarkFunds + transportFunds + utilitiesFunds;
+        CHECK(totalAllocation <= 1000000.0);
+    }
 
-        // Set tax rates
-        financeDept.setResidentialIncomeTaxRate(0.2);   // 20%
-        financeDept.setResidentialPropertyTaxRate(0.1); // 10%
-        financeDept.setCommercialBusinessTaxRate(0.25); // 25%
-        financeDept.setCommercialSalesTaxRate(0.15);    // 15%
+    delete financeDept;
+}
 
-        // Set initial available funds
-        financeDept.setAvailableFunds(1000000.0);
+TEST_CASE("Testing Government") {
+    FinanceDepartment* financeDept = new FinanceDepartment();
+    Government* government = new Government(financeDept);
+    
+    /*SUBCASE("Government Initialization") {
+        CHECK(government != nullptr);
+        CHECK(government->getFinanceDepartment() != nullptr);
+    }
 
-        // Test tax collection methods
-        SUBCASE("Income Tax Collection") {
-            double collectedTax = financeDept.delegateRequestForCollectionOffIncomeTax();
-            CHECK_EQ(collectedTax, doctest::Approx(10000.0)); // 20% of 50000.0
+    SUBCASE("Citizen Attachment") {
+        for(int i = 0; i < 10; i++) {
+            CitizenInterface* citizen = new Citizen();
+            government->attach(citizen);
+        }
+        CHECK(government->getNumberOfCitizens() == 10);
+    }*/
+
+    SUBCASE("Budget Setting and Getting") {
+        double testBudget = 40000000.00;
+        government->setBudget(testBudget);
+        CHECK(government->getBudget() == doctest::Approx(testBudget));
+    }
+
+    SUBCASE("Tax Collection") {
+        financeDept->setResidentialIncomeTaxRate(0.10);
+        financeDept->setResidentialPropertyTaxRate(0.012);
+        financeDept->setCommercialBusinessTaxRate(0.25);
+        financeDept->setCommercialSalesTaxRate(0.08);
+        financeDept->setAvailableFunds(40000000.00);
+
+        double propertyTaxCollected = government->requestCollectionOfPropertyTax();
+        double incomeTaxCollected = government->requestCollectionOfIncomeTax();
+        double businessTaxCollected = government->requestCollectionOfBusinessTax();
+        double salesTaxCollected = government->requestCollectionOfSalesTax();
+
+        CHECK(propertyTaxCollected >= 0);
+        CHECK(incomeTaxCollected >= 0);
+        CHECK(businessTaxCollected >= 0);
+        CHECK(salesTaxCollected >= 0);
+    }
+
+    SUBCASE("Fund Allocation") {
+        government->setBudget(40000000.00);
+
+        double utilitiesFunds = government->requestAllocationOfUtilitiesFunds();
+        double publicServiceFunds = government->requestAllocationOfPublicServiceBuildingsFunds();
+        double transportFunds = government->requestAllocationOfTransportFunds();
+        double educationFunds = government->requestAllocationOfEducationFunds();
+        double recreationFunds = government->requestAllocationOfRecreationFunds();
+
+        CHECK(utilitiesFunds >= 0);
+        CHECK(publicServiceFunds >= 0);
+        CHECK(transportFunds >= 0);
+        CHECK(educationFunds >= 0);
+        CHECK(recreationFunds >= 0);
+
+        double totalAllocation = utilitiesFunds + publicServiceFunds + transportFunds + 
+                               educationFunds + recreationFunds;
+        CHECK(totalAllocation <= government->getBudget());
+    }
+
+    SUBCASE("Integrated Tax Collection and Fund Allocation") {
+        government->setBudget(40000000.00);
+        financeDept->setResidentialIncomeTaxRate(0.10);
+        financeDept->setResidentialPropertyTaxRate(0.012);
+        financeDept->setCommercialBusinessTaxRate(0.25);
+        financeDept->setCommercialSalesTaxRate(0.08);
+
+        for(int i = 0; i < 5; i++) {
+            CitizenInterface* citizen = new Citizen();
+            government->attach(citizen);
         }
 
-        SUBCASE("Business Tax Collection") {
-            double collectedTax = financeDept.delegateRequestForCollectionOfBusinessTax();
-            CHECK_EQ(collectedTax, doctest::Approx(50000.0)); // 25% of 200000.0
-        }
+        double totalTaxes = government->requestCollectionOfPropertyTax() +
+                           government->requestCollectionOfIncomeTax() +
+                           government->requestCollectionOfBusinessTax() +
+                           government->requestCollectionOfSalesTax();
 
-        // Clean up memory (in a real scenario, use smart pointers)
-        for(auto resident : residents) delete resident;
-        for(auto building : buildings) delete building;
+        double totalAllocations = government->requestAllocationOfUtilitiesFunds() +
+                                government->requestAllocationOfPublicServiceBuildingsFunds() +
+                                government->requestAllocationOfTransportFunds() +
+                                government->requestAllocationOfEducationFunds() +
+                                government->requestAllocationOfRecreationFunds();
+
+        CHECK(totalTaxes >= 0);
+        CHECK(totalAllocations >= 0);
+        CHECK(totalAllocations <= government->getBudget());
     }
-}
 
-// Government Tests (Minimal mock setup)
-TEST_CASE("Government") {
-    // Mock FinanceDepartment for testing
-    class MockFinanceDepartment : public FinanceDepartment {
-    public:
-        double getAvailableFunds() override { return 1000000.0; }
-        double delegateRequestForCollectionOffIncomeTax() override { return 50000.0; }
-        // Implement other mocked methods as needed
-    };
-
-    SUBCASE("Budget and Revenue Management") {
-        MockFinanceDepartment mockFinanceDept;
-        Government govt(&mockFinanceDept);
-
-        // Test budget and revenue methods
-        govt.setBudget(500000.0);
-        CHECK_EQ(govt.getBudget(), 500000.0);
-
-        govt.setTotalRevenue(1000000.0);
-        CHECK_EQ(govt.getTotalRevenue(), 1000000.0);
-    }
+    delete government;
+    delete financeDept;
 }
